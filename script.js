aaaa5b1266325e1c902f1da0f03b9edacc3fda37
G S <35782741-Giselle-S@users.noreply.replit.com>
2025-07-07 20:34:56 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+/* VARIABLES */
+
+/* SETUP RUNS ONCE */
+function setup() {
+  //sets the screen size
+  createCanvas(400,400); 
+
+  //sets the background color
+  background(255,249,226); 
+}
+
+/* DRAW LOOP REPEATS */
+function draw() {
+  
+}
